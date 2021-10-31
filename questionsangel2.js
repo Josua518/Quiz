1fe722feb1b571dfd5414c0d00f55de7ebe7babe
Josua518 <84269701+Josua518@users.noreply.github.com>
2021-10-31 10:33:55 +0700

    Add files via upload

---
+++ questionsangel2.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1,
+  question: "Hasil perkalian dari 100 × 50 adalah ...",
+  answer: "5.000",
+  options: [
+    "3.000",
+    "4.000",
+    "5.000",
+    "6.000"
+  ]
+},
+  {
+    numb: 2,
+    question: "Hasil perkalian dari 124 × 47 adalah ...",
+    answer: "5.828",
+    options: [
+      "5.728",
+      "5.828",
+      "5.928",
+      "6.028"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Hasil perkalian dari 387 × 74 adalah ...",
+    answer: "28.638",
+    options: [
+      "28.238",
+      "28.338",
+      "28.538",
+      "28.638"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Sebuah kertas berbentuk lingkaran dengan keliling 616 cm. Diameternya adalah .... cm",
+    answer: "196",
+    options: [
+      "196",
+      "198",
+      "208",
+      "216"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Tina memiliki hulahop dengan keliling 220 cm. Jari-jari hulahop Tina adalah .... cm",
+    answer: "35",
+    options: [
+      "28",
+      "30",
+      "32",
+      "35"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Luas sebuah lingkaran adalah 2.464 cm². Keliling lingkaran tersebut adalah .... cm",
+    answer: "L = Л x r² dan K = 2 x Л x r",
+    options: [
+      "168",
+      "174",
+      "176",
+      "182"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Luas dan keliling bangun yang diketahui D = 35Cm adalah ....",
+    answer: "88",
+    options: [
+      "Luas bangun = 481,15 cm² , kelilingnya = 85 cm",
+      "Luas bangun = 481,25 cm² , kelilingnya = 90 cm",
+      "Luas bangun = 481,50 cm² , kelilingnya = 92 cm",
+      "Luas bangun = 481,75 cm² , kelilingnya = 95 cm"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Sebuah taman berbentuk lingkaran, kelilingnya adalah 3.850 m. Diameter taman tersebut adalah .... m.",
+    answer: "24,5",
+    options: [
+      "1.200",
+      "1.220",
+      "1.225",
+      "1.230"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Ibu membuat taplak meja berbentuk lingkaran berdiameter 1,4 m. Setelah jadi, ibu mengukur keliling taplak meja tersebut dan ternyata panjangnya adalah .... meter.",
+    answer: "196",
+    options: [
+      "3,5",
+      "3,75",
+      "4",
+      "4,15"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Sebuah lapangan berbentuk lingkaran berdiameter 60 m. Andi berlari mengelilingi lapangan tersebut 3 kali. Maka jarak yang ditempuh Andi adalah .... meter.",
+    answer: "35",
+    options: [
+      "562,5",
+      "565,2",
+      "565,5",
+      "565,8"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
