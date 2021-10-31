02640a7a98a63ceb8c459dabb76c6328ffda8780
Josua518 <84269701+Josua518@users.noreply.github.com>
2021-10-31 10:35:15 +0700

    Add files via upload

---
+++ questionsatan1.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1,
+  question: "Rani membeli 3 bungkus permen. Setiap bungkus berisi 9 permen. Jumlah seluruh permen rani adalah ...",
+  answer: "27 Permen",
+  options: [
+    "25 Permen",
+    "27 Permen",
+    "30 Permen",
+    "32 Permen"
+  ]
+},
+  {
+    numb: 2,
+    question: "Pak Budi membeli 5 1/2 lusin pensil. Seluruh pensil tersebut akan dibagikan kepada beberapa muridnya. Setiap anak mendapat 1/6 lusin. Berapa anak yang mendapatkan pensil?",
+    answer: "33 Pensil",
+    options: [
+      "30 Pensil",
+      "31 Pensil",
+      "32 Pensil",
+      "33 Pensil"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Firyal membeli pita di Toko Pak Eno sepanjang 5 meter. Pita tersebut akan dipotong-potong dengan ukuran yang sama panjang. Setiap potongan panjangnya adalah 1/5 meter. Berapa banyak potongan pita tersebut?",
+    answer: "25",
+    options: [
+      "20",
+      "25",
+      "30",
+      "35"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Seorang pedagang membeli gula sebanyak 20 kg. Gula itu selanjutnya akan dibungkus ke dalam plastik-plastik kecil. Setiap plastik kecil berisi 1 1/4 kg. Berapa banyak plastik kecil yang dibutuhkan pedagang tersebut?",
+    answer: "16",
+    options: [
+      "13",
+      "16",
+      "19",
+      "21"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Persediaan beras Ibu saat ini adalah 21 kg. Setiap hari Ibu menghabiskan beras untuk memasak sebanyak 3/4 kg. Berapa hari persediaan beras Ibu akan habis?",
+    answer: "28",
+    options: [
+      "25",
+      "28",
+      "31",
+      "35"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Sebuah mobil pick up akan mengangkut pasir sebanyak 7 ton. Setiap kali angkut, mobil hanya mampu membawa 2 1/3 kuintal pasir. Berapa kali mobil pick up tersebut dapat mengangkut semua pasir?",
+    answer: "3",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Ibu mempunyai susu 1 1/5 liter. susu tersebut akan dimasukkan ke dalam gelas. Setiap gelas berisi 1/5 liter. Berapa gelas yang dibutuhkan Ibu?",
+    answer: "6",
+    options: [
+      "3",
+      "4",
+      "5",
+      "6"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Siti memiliki pita 3/4 meter, sedangkan Beni memiliki pita 7/8 meter. Jika pita mereka disambung, maka panjang maksimal hasil pita sambungan adalah … meter.",
+    answer: "1 5/8",
+    options: [
+      "5/8",
+      "10/8",
+      "1 5/8",
+      "1 1/8"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Pada penimbangan bayi di posyandu diperoleh data berikut. Berat Aira 26/4 kg, berat Meyza 27/5 kg, dan berat Zaskia 23/4 kg. Tentukan berat ketiga bayi tersebut!",
+    answer: "17 13/20",
+    options: [
+      "17 3/20",
+      "17 13/20",
+      "13/20",
+      "3/20"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Adi memiliki sebuah benang layangan yang panjangnya 51/2 . setelah itu adi membeli benang lagi yang panjagnya 20 meter. Berapa jumlah benang adi saat ini?",
+    answer: "25 1/2",
+    options: [
+      "25 1/2",
+      "25 1/3",
+      "25 1/4",
+      "25 1/5"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ questionsatan2.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1,
+  question: "Perbandingan permen Cika dan Risa adalah 5 : 8, Jika permen Cika dan Risa adalah 156. Jadi berapa banyak permen Risa?",
+  answer: "96 Permen",
+  options: [
+    "90 Permen",
+    "93 Permen",
+    "96 Permen",
+    "99 Permen"
+  ]
+},
+  {
+    numb: 2,
+    question: "Kelereng Arman dan Marta berjumlah sebanyak 180 butir. Perbandingan dari kelereng Arman dan Marta 4 : 5, Maka jumlah kelereng Arman dan Marta adalah  ...",
+    answer: "Kelereng Arman = 80 dan kelereng Marta = 100",
+    options: [
+      "Kelereng Arman = 60 dan kelereng Marta = 120",
+      "Kelereng Arman = 70 dan kelereng Marta = 110",
+      "Kelereng Arman = 80 dan kelereng Marta = 100",
+      "Kelereng Arman = 85 dan kelereng Marta = 95"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Perbandingan usia antara Ardi dan Intan adalah 3 : 5, Jika usia Ardi adalah 6 Tahun, Berapakah usia Intan?",
+    answer: "10 Tahun",
+    options: [
+      "10 Tahun",
+      "15 Tahun",
+      "20 Tahun",
+      "25 Tahun"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Budi berumur : umur Alif 9 : 8. Jika selisih dari umur Budi dan Alif 5 tahun. Umur dari Budi dan Alif sekarang adalah ...",
+    answer: "Umur Budi 45, umur Alif 40",
+    options: [
+      "Umur Budi 35, umur Alif 30",
+      "Umur Budi 40, umur Alif 35",
+      "Umur Budi 45, umur Alif 40",
+      "Umur Budi 50, umur Alif 45"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Uang Rada dan Fani mempunyai perbandingan  3 : 4. Maka apabila uang Rada  Rp 225.000,00 maka uang Fani adalah ...",
+    answer: "Rp 300.000,00",
+    options: [
+      "Rp 250.000,00",
+      "Rp 275.000,00",
+      "Rp 280.000,00",
+      "Rp 300.000,00"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Uang Galuh dan Dewi berjumlah Rp 300.000. Apabila uang Galuh Rp 175.000,00,  Jadi berapakah perbandingan uang mereka ...",
+    answer: "7 : 5",
+    options: [
+      "5 : 3",
+      "6 : 5",
+      "7 : 5",
+      "7 : 6"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Umur seorang kakek adalah tiga kali umur ayahku. Jadi pada suatu saat umur kakek dan ayah 80 tahun, maka umur ayah ...",
+    answer: "20",
+    options: [
+      "10",
+      "12",
+      "20",
+      "25"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Setelah selama 7 hari bekerja, Randa memperoleh gaji Rp 350.000,00. Setelah selama 9 hari bekerja, Randa mendapat gaji... meter.",
+    answer: "1 5/8",
+    options: [
+      "Rp 450.000,00",
+      "Rp 500.000,00",
+      "Rp 550.000,00",
+      "Rp 600.000,00"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Diketahui 2/3 uang Azzy Rp 75.000,00. Uang Azzy mula-mula adalah ...",
+    answer: "17 13/20",
+    options: [
+      "Rp 110.000,00",
+      "Rp 111.500,00",
+      "Rp 112.000,00",
+      "Rp 112.500,00"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Didalam sebuah kotak terdapat 54 bola yang terdiri dari 36 bola yang berwarna pink dan sisanya mempunyai warna putih. Jadi berapakah jumlah Perbandingan bola putih terhadap pada jumlah seluruh bolanya ...",
+    answer: "1 : 8",
+    options: [
+      "3 : 4",
+      "6 : 7",
+      "5 : 8",
+      "1 : 8"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
