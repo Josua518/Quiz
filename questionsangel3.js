db3539cac305e7a19fc260b5d518141912cb40cd
Josua518 <84269701+Josua518@users.noreply.github.com>
2021-10-31 11:34:01 +0700

    Add files via upload

---
+++ questionsangel3.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1,
+  question: "Rumus dari keliling lingkaran jika diketahui jari jari adalah ...",
+  answer: "K = Л x r²",
+  options: [
+    "K = 2 x Л x r",
+    "K = 2 x Л x d",
+    "K = Л x r²",
+    "K = Л x d²"
+  ]
+},
+  {
+    numb: 2,
+    question: "Sebuah lingkaran dengan panjang jari jari 7 cm. Maka keliling lingkaran tersebut adalah ... cm",
+    answer: "44",
+    options: [
+      "22",
+      "44",
+      "88",
+      "176"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Sebuah piring snack berbentuk lingkaran dengan panjang jari jari 10 cm. Maka keliling piring snack tersebut adalah ... cm",
+    answer: "62,8",
+    options: [
+      "31,4",
+      "62,8",
+      "314",
+      "628"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Rumus lingkaran jika diketahui diameter adalah ...",
+    answer: "K = Л x d",
+    options: [
+      "K = Л x d",
+      "K = Л x d²",
+      "K = 2 × Л x d",
+      "K = 2 × Л x d²"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Panjang diameter sebuah lingkaran adalah 28 cm. Maka keliling lingkaran itu adalah ... cm",
+    answer: "88",
+    options: [
+      "44",
+      "88",
+      "154",
+      "616"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Panjang diameter sebuah lingkaran adalah 56 cm. Maka keliling lingkaran itu adalah ... cm",
+    answer: "176",
+    options: [
+      "55",
+      "99",
+      "176",
+      "626"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Panjang diameter sebuah lingkaran adalah 63 cm. Maka keliling lingkaran itu adalah ... cm",
+    answer: "198",
+    options: [
+      "176",
+      "198",
+      "201",
+      "221"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Panjang diameter sebuah lingkaran adalah 140 cm. Maka keliling lingkaran itu adalah ... cm",
+    answer: "440",
+    options: [
+      "280",
+      "440",
+      "660",
+      "810"
+    ]
+  },
+  {
+    numb: 9,
+    question: "2,74 × 3 =",
+    answer: "8,22",
+    options: [
+      "7,22",
+      "8,22",
+      "9,22",
+      "10,22"
+    ]
+  },
+  {
+    numb: 10,
+    question: "6,28 ÷ 2 = ",
+    answer: "3,14",
+    options: [
+      "1,57",
+      "3,14",
+      "12,56",
+      "15,78"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
