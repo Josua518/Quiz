c4b8fbf212b04eb11fab636e90bb0085b51229c7
Josua518 <84269701+Josua518@users.noreply.github.com>
2021-11-01 09:21:36 +0700

    Add files via upload

---
+++ questionsatan3.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1,
+  question: "Diketahui A : B = 5 : 6. Jika nilai A = 20, maka nilai B adalah ...",
+  answer: "24",
+  options: [
+    "20",
+    "24",
+    "28",
+    "32"
+  ]
+},
+  {
+    numb: 2,
+    question: "Siswa kelas VI SD Sukamaju terdiri dari 15 siswa laki-laki dan 20 siswa perempuan. Perbandingan siswa laki-laki dan perempuan adalah ...",
+    answer: "3 : 4",
+    options: [
+      "2 : 3",
+      "3 : 4",
+      "4 : 5",
+      "5 : 6"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Siswa kelas VI SD Budi Agung terdiri dari 30 siswa laki-laki dan 20 siswa perempuan. Perbandingan siswa laki-laki dan perempuan adalah ....",
+    answer: "3 : 2",
+    options: [
+      "1 : 2",
+      "2 : 2",
+      "3 : 2",
+      "4 : 2"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Siswa kelas VI terdiri dari 5 siswa laki-laki dan 10 siswa perempuan. Perbandingan siswa laki-laki dan perempuan adalah ....",
+    answer: "1 : 2",
+    options: [
+      "1 : 1",
+      "1 : 2",
+      "2 : 1",
+      "2 : 2"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Uang Rada dan Fani mempunyai perbandingan  3 : 4. Maka apabila uang mereka  Rp 350.000 maka uang Fani adalah ...",
+    answer: "Rp 280.000,00",
+    options: [
+      "Rp 250.000,00",
+      "Rp 275.000,00",
+      "Rp 280.000,00",
+      "Rp 300.000,00"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Kelereng Arman dan Rudi berjumlah 180 butir. Perbandingan kelereng Arman dan Rudi 4 : 5. Jumlah kelereng masing-masing adalah ...",
+    answer: "7 : 5",
+    options: [
+      "5 : 3",
+      "6 : 5",
+      "7 : 5",
+      "7 : 6"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
