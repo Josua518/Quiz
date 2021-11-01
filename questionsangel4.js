34521a5dbc07b053f18b63dbb228bb352e9feb1b
Josua518 <84269701+Josua518@users.noreply.github.com>
2021-11-01 08:10:38 +0700

    Add files via upload

---
+++ questionsangel4.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1,
+  question: "Rumus luas lingkaran jika diketahui jari jari adalah ...",
+  answer: "L = π × r²",
+  options: [
+    "L = π × r",
+    "L = π × r²",
+    "L = 1/2 × π × r²",
+    "K = 2 × Л x r"
+  ]
+},
+  {
+    numb: 2,
+    question: "Rumus luas lingkaran jika diketahui diamater adalah ...",
+    answer: "L = 1/4 × π × D²",
+    options: [
+      "L = 1/4 × π × D²",
+      "L = 1/2 × π × D²",
+      "L = 1/4 × π × D",
+      "L = 1/2 × π × D"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Diketahui sebuah lingkaran memiliki diameter 28 cm. Berapakah luas lingkaran tersebut?",
+    answer: "616 cm²",
+    options: [
+      "610 cm²",
+      "616 cm²",
+      "619 cm²",
+      "621 cm²"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Sebuah roda sepeda yang berbentuk lingkaran memiliki diameter sebesar 14 cm. Maka luas roda tersebut adalah ?",
+    answer: "154 cm²",
+    options: [
+      "154 cm²",
+      "512 cm²",
+      "654 cm²",
+      "616 cm²"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Tentukan diameter lingkaran jika diketahui jari-jari 4 cm ?",
+    answer: "8 cm",
+    options: [
+      "2 cm",
+      "4 cm",
+      "6 cm",
+      "8 cm"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Sebuah meja yg berbentuk lingkaran memiliki diameter 1,4 meter . Di atas meja tersebut akan dipasang kaca sesuai dengan luas meja tersebut. Tentukan luas kaca yg diperlukan ?",
+    answer: "1,54 cm",
+    options: [
+      "1,54 cm",
+      "3,08 cm",
+      "6,16 cm",
+      "12,32 cm"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Sebuah lingkaran memiliki luas 154 cm2. Berapa jari-jari lingkaran tersebut?",
+    answer: "7 cm",
+    options: [
+      "6 cm",
+      "7 cm",
+      "8 cm",
+      "9 cm"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Keliling dari sebuah lingkaran adalah 314 cm. Hitunglah diameter lingkaran tersebut!",
+    answer: "100 cm",
+    options: [
+      "25 cm",
+      "50 cm",
+      "75 cm",
+      "100 cm"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Sebuah taman berbentuk lingkaran dengan panjang diameternya 10 meter. Tentukan luas lingkaran!",
+    answer: "78,5 cm²",
+    options: [
+      "72,2 cm²",
+      "78,5 cm²",
+      "81,1 cm²",
+      "90,8 cm²"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Keliling lingkaran 132 cm, berapakah luas lingkaran tersebut?",
+    answer: "1.386 cm²",
+    options: [
+      "1.386 cm²",
+      "1.486 cm²",
+      "1.586 cm²",
+      "1.686 cm²"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
